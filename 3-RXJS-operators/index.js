018383b463c08a0691bf9d6d794012de1ad5b09d
Sahil Kandhare <skandhare_comp@jspmrscoe.edu.in>
2024-10-12 17:23:52 +0530

    ✨ Add index

---
+++ 3-RXJS-operators/index.js
@@ -0,0 +1,36 @@
+import { createAndSubscribe } from "./1_observable.js";
+import { milestoneTap } from "./2_tap.js";
+import { milestoneMap } from "./3_map.js";
+import { milestoneFilter } from "./4_filter.js";
+import { milestoneZip } from "./5_zip.js";
+import { milestoneCombineLatest } from "./6_combineLatest.js";
+import { milestoneForkJoin } from "./7_forkJoin.js";
+import { milestoneSwichMap } from "./8_switchMap.js";
+import { milestoneConcatMap } from "./9_concatMap.js";
+
+// Milestone 1: Creating Observables and Subscribing
+// createAndSubscribe();
+
+// Milestone 2: Learning tap operator
+// milestoneTap();
+
+// Milestone 3: Learning map operator
+// milestoneMap();
+
+// Milestone 4: Learning filter operator
+// milestoneFilter();
+
+// Milestone 5: Learning zip operator
+// milestoneZip();
+
+// Milestone 6: Learning combineLatest operator
+// milestoneCombineLatest();
+
+// Milestone 7: Learning forkJoin operator
+// milestoneForkJoin();
+
+// Milestone 8: Learning switchMap operator
+// milestoneSwichMap();
+
+// Milestone 9: Learning concatMap operator
+milestoneConcatMap();
